cfc22a03d48e528ba491f958b97e25ea0eef3567
Diego <sdieg82@gmail.com>
2025-08-11 09:31:46 -0500

    readable views

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tok_tik_app/Config/helpers/human_formats.dart';
 import 'package:tok_tik_app/domain/entities/video_post.dart';
 
 class VideoButtons extends StatelessWidget {
@@ -12,12 +13,42 @@ class VideoButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-    
+      children: [
+        _CustomIconButton(
+          value: videos.likes, 
+          icondata: Icons.favorite, 
+          iconColor: Colors.red
+        ),
+        _CustomIconButton(
+          value: videos.likes, 
+          icondata: Icons.remove_red_eye_outlined,
+        ),
+      ],
+    );
+  }
+}
+
+
+class _CustomIconButton extends StatelessWidget {
+  final int value;
+  final IconData icondata;
+  final Color? color;
+  const _CustomIconButton({
+    super.key, 
+    required this.value, 
+    required this.icondata, 
+    iconColor
+  }):color=iconColor ?? Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       children: [
         IconButton(
           onPressed: (){}, 
-          icon: Icon(Icons.favorite,color:Colors.red
+          icon: Icon(icondata,color:color,size: 30,
         )),
+        Text(HumanFormats.numberFormat(value.toDouble()))
       ]
     );
   }
